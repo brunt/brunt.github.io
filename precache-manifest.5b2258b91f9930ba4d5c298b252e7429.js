d190424aee1045729e9a7270f3b3f8155839d320
bryantdeters <bryant.j.deters@centene.com>
2019-11-18 17:27:30 -0600

    wasm-sudoku: pwa edition

---
+++ precache-manifest.5b2258b91f9930ba4d5c298b252e7429.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdfef4278358885919c",
+    "url": "0.bootstrap.js"
+  },
+  {
+    "revision": "2bdfef4278358885919c",
+    "url": "991f97e0b030a596210b.module.wasm"
+  },
+  {
+    "revision": "9f2b6176ccd5ffcdab151d43f9b6e01b",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "c8eaf3a4796dd27eb6be13d031b9372e",
+    "url": "android-chrome-256x256.png"
+  },
+  {
+    "revision": "30935fd3fb6dfb187e122cd46835e7f8",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "4b921f6860590f816bd8",
+    "url": "bootstrap.js"
+  },
+  {
+    "revision": "0fc0db4727e40770ca347689fc32485f",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "8d69e35be1d19b5d1755e34a65e46498",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "ecd9d2234c83a2d55ef7a5febe2798fb",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "b15ea2470b54ffe72014d6851b071ecc",
+    "url": "index.html"
+  },
+  {
+    "revision": "e7c575a5a1bea86d78cf77091d0900e6",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "revision": "5bca67c2425fc192e93c7476007ce3d8",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "32c3498c19824fe319a6a39b45c56b7f",
+    "url": "styles.css"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-importScripts("precache-manifest.95c54a113f05eb9abf8dde5eb4b19b1f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.5b2258b91f9930ba4d5c298b252e7429.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 workbox.routing.registerRoute(
-    /\./,
+    /\//,
     new workbox.strategies.CacheFirst({
         cacheName: "wasm-sudoku",
         plugins: [
